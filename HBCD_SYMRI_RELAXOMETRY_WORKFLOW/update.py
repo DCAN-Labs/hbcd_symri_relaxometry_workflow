24033ac8712ada1af9903e767ff3b10c0db0e53a
Erik Lee <erik.lee.mn@gmail.com>
2026-01-20 10:57:22 -0600

    Allow P/V session labels with length 3 or 4

--- HBCD_SYMRI_RELAXOMETRY_WORKFLOW/update.py
+++ HBCD_SYMRI_RELAXOMETRY_WORKFLOW/update.py
@@ -461,7 +461,7 @@ def targz_to_sub_ses_labels(targz_name):
     
     split_name = targz_name.split('/')[-1].split('_')
     if len(split_name) >= 3:
-        if (len(split_name[0]) == 9) and (len(split_name[1]) == 6) and (len(split_name[2]) == 3) and (split_name[2][0] == 'V'):
+        if (len(split_name[0]) == 9) and (len(split_name[1]) == 6) and ((len(split_name[2]) == 3) or (len(split_name[2]) == 4)) and (split_name[2][0] in ['P', 'V']):
             if len(split_name) == 3:
                 sub_label = split_name[1]
                 ses_label = split_name[2].split('.')[0]
@@ -1104,4 +1104,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
